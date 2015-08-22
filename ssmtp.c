fdev1/ssmtp-reloaded

commit 1602b6dcb3c7a4fba9348a2d3b7a0f90f433efbc

Allow spaces in password
Fixes problem with spaces in password as well as
compile warnings when compiling without gnutls.

ssmtp.c
@@ -49,6 +49,7 @@
 #include <sys/wait.h>
 #include <paths.h>
 #include <errno.h>
+#include <time.h>
 
 bool_t have_date = False;
 bool_t have_from = False;
@@ -1442,11 +1443,13 @@ bool_t read_config()
 				}
 			}
 			else if(strcasecmp(p, "AuthPass") == 0 && !auth_pass) {
-				auth_pass = firsttok(&rightside, " \n\t");
-				if(auth_pass  == (char *)NULL) {
-					die("parse_config() -- strdup() failed");
-				}
-
+				while(*rightside == ' ' || *rightside == '\t')
+					rightside++;
+				auth_pass = rightside;
+				while(*rightside != '\0')
+					rightside++;
+				while(isspace(*--rightside))
+					*rightside = '\0';
 				if(log_level > 0) {
 					#if 0
 					log_event(LOG_INFO, "Set AuthPass=\"%s\"\n", auth_pass);
@@ -1613,8 +1616,13 @@ int smtp_open(char *host, int port)
 
 	/* Init SSL stuff */
 	SSL_CTX *ctx;
+	#ifdef HAVE_GNUTLS
 	SSL_METHOD *meth;
 	const X509 *server_cert;
+	#else
+	const SSL_METHOD *meth;
+	X509 *server_cert;
+	#endif
 
 	SSL_load_error_strings();
 	SSLeay_add_ssl_algorithms();

configure
@@ -622,7 +622,6 @@ ac_includes_default="\
 ac_subst_vars='LTLIBOBJS
 LIBOBJS
 SRCS
-queuedir
 EGREP
 GREP
 CPP
@@ -686,7 +685,6 @@ enable_ssl
 with_gnutls
 enable_inet6
 enable_md5auth
-with_queue_dir
 '
       ac_precious_vars='build_alias
 host_alias
@@ -1315,7 +1313,6 @@ Optional Packages:
   --with-PACKAGE[=ARG]    use PACKAGE [ARG=yes]
   --without-PACKAGE       do not use PACKAGE (same as --with-PACKAGE=no)
   --with-gnutls           Use GnuTLS for SSL support
-  --with-queue-dir=dir	  default queue directory (default /var/ssmtp/queue)
 
 Some influential environment variables:
   CC          C compiler command
@@ -3538,7 +3535,8 @@ fi
 done
 
 
-for ac_header in limits.h strings.h syslog.h unistd.h
+for ac_header in limits.h strings.h syslog.h unistd.h time.h \
+	errno.h paths.h sys/wait.h langinfo.h dirent.h fcntl.h
 do :
   as_ac_Header=`$as_echo "ac_cv_header_$ac_header" | $as_tr_sh`
 ac_fn_c_check_header_mongrel "$LINENO" "$ac_header" "$as_ac_Header" "$ac_includes_default"
@@ -3943,22 +3941,6 @@ if test x$enableval = xyes ; then
 	SRCS="$SRCS md5auth/md5c.c md5auth/hmac_md5.c"
 fi
 enableval=""
-
-
-# Check whether --with-queue-dir was given.
-if test "${with_queue_dir+set}" = set; then :
-  withval=$with_queue_dir;
-fi
-
-if test x$withval = xyes ; then
-	queuedir="/var/ssmtp/queue"
-elif test x$withval = xno ; then
-	as_fn_error $? "Invalid option: --without-queue-dir" "$LINENO" 5
-else
-	queuedir="$withval"
-fi
-withval=""
-
 
 
 

configure.ac
@@ -12,7 +12,8 @@ dnl Checks for libraries.
 
 dnl Checks for header files.
 AC_HEADER_STDC
-AC_CHECK_HEADERS(limits.h strings.h syslog.h unistd.h)
+AC_CHECK_HEADERS(limits.h strings.h syslog.h unistd.h time.h \
+	errno.h paths.h sys/wait.h langinfo.h dirent.h fcntl.h)
 
 
 AC_CACHE_CHECK([for obsolete openlog],ssmtp_cv_obsolete_openlog,

commit 12b681abaa2e25ebb97f2526497cef95942dd7da

Fix password parsing bug
Fix bug introduced by commit
1602b6dcb3c7a4fba9348a2d3b7a0f90f433efbc

ssmtp.c
@@ -1441,7 +1441,7 @@ bool_t read_config()
 				while(*rightside == ' ' || *rightside == '\t') {
 					rightside++;
 				}
-				auth_pass = rightside;
+				auth_pass = strdup(rightside);
 				while(*rightside != '\0') {
 					rightside++;
 				}